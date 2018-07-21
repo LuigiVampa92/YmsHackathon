3656bc4fb46fce0bb17e84660655b5ef9225ad5b
yura_sultonov <yura_sultonov@mail.ru>
2018-07-21 18:34:01 +0300

    Best result window completed

--- app/src/main/java/com/luigivampa92/yms/timekiller/ui/main/MainActivity.kt
+++ app/src/main/java/com/luigivampa92/yms/timekiller/ui/main/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.luigivampa92.yms.timekiller.ui.main
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -12,6 +15,10 @@ import com.luigivampa92.yms.timekiller.R
 import com.luigivampa92.yms.timekiller.model.entity.Letter
 import com.luigivampa92.yms.timekiller.ui.base.BaseActivity
 import javax.inject.Inject
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+
 
 class MainActivity : BaseActivity(), MainView {
 
@@ -25,6 +32,8 @@ class MainActivity : BaseActivity(), MainView {
     protected lateinit var recyclerViewWord: RecyclerView
     @BindView(R.id.recycler_view_field)
     protected lateinit var recyclerViewField: RecyclerView
+    @BindView(R.id.toolbar)
+    protected lateinit var toolbar: Toolbar
 
     private lateinit var wordAdapter: WordRecyclerViewAdapter
     private lateinit var wordLayoutManager: RecyclerView.LayoutManager
@@ -45,6 +54,27 @@ class MainActivity : BaseActivity(), MainView {
         fieldLayoutManager = GridLayoutManager(this, 8)
         recyclerViewField.adapter = fieldAdapter
         recyclerViewField.layoutManager = fieldLayoutManager
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_result -> {
+                val intent = Intent(this, ResultActivity::class.java)
+                startActivity(intent)
+            }
+            else -> {
+            }
+        }
+
+        return true
     }
 
     override fun onResume() {
