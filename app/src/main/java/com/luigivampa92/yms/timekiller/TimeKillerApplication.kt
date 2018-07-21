95f1f86c8b682d04b9b5c54adcf25873e7d795eb
Pavel Vasiliev <luigivampa92@gmail.com>
2018-07-21 11:51:27 +0300

    First commit

---
+++ app/src/main/java/com/luigivampa92/yms/timekiller/TimeKillerApplication.kt
@@ -0,0 +1,10 @@
+package com.luigivampa92.yms.timekiller
+
+import android.app.Application
+
+class TimeKillerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
