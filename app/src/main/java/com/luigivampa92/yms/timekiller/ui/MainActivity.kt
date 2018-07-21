6393b70d25159cdefc3590072f7d6254539f04b4
Pavel Vasiliev <luigivampa92@gmail.com>
2018-07-21 12:04:12 +0300

    removed unused resources

--- app/src/main/java/com/luigivampa92/yms/timekiller/ui/MainActivity.kt
+++ app/src/main/java/com/luigivampa92/yms/timekiller/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.luigivampa92.yms.timekiller
+package com.luigivampa92.yms.timekiller.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.luigivampa92.yms.timekiller.R
 
 class MainActivity : AppCompatActivity() {
 
