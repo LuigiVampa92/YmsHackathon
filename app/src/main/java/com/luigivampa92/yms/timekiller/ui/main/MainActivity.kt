55ee3f85f77ace97ed07a56141e9da38c1c8d7cd
Pavel Vasiliev <luigivampa92@gmail.com>
2018-07-21 15:16:18 +0300

    added field recycler on view

--- app/src/main/java/com/luigivampa92/yms/timekiller/ui/main/MainActivity.kt
+++ app/src/main/java/com/luigivampa92/yms/timekiller/ui/main/MainActivity.kt
@@ -2,13 +2,13 @@ package com.luigivampa92.yms.timekiller.ui.main
 
 import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.luigivampa92.yms.timekiller.R
-import com.luigivampa92.yms.timekiller.model.entity.GameField
 import com.luigivampa92.yms.timekiller.model.entity.Letter
 import com.luigivampa92.yms.timekiller.ui.base.BaseActivity
 import javax.inject.Inject
@@ -28,16 +28,23 @@ class MainActivity : BaseActivity(), MainView {
 
     private lateinit var wordAdapter: WordRecyclerViewAdapter
     private lateinit var wordLayoutManager: RecyclerView.LayoutManager
+    private lateinit var fieldAdapter: FieldRecyclerViewAdapter
+    private lateinit var fieldLayoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
 
-        wordAdapter = WordRecyclerViewAdapter {}
+        wordAdapter = WordRecyclerViewAdapter(presenter::wordLetterClicked)
         wordLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerViewWord.adapter = wordAdapter
         recyclerViewWord.layoutManager = wordLayoutManager
+
+        fieldAdapter = FieldRecyclerViewAdapter(presenter::fieldLetterClicked)
+        fieldLayoutManager = GridLayoutManager(this, 8)
+        recyclerViewField.adapter = fieldAdapter
+        recyclerViewField.layoutManager = fieldLayoutManager
     }
 
     override fun onResume() {
@@ -50,7 +57,7 @@ class MainActivity : BaseActivity(), MainView {
         wordAdapter.setItems(letter)
     }
 
-    override fun setField(field: GameField) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun setField(field: List<Letter>) {
+        fieldAdapter.showField(field)
     }
 }
--- app/src/main/java/com/luigivampa92/yms/timekiller/ui/main/MainPresenter.kt
+++ app/src/main/java/com/luigivampa92/yms/timekiller/ui/main/MainPresenter.kt
@@ -2,17 +2,120 @@ package com.luigivampa92.yms.timekiller.ui.main
 
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
+import com.luigivampa92.yms.timekiller.log
 import com.luigivampa92.yms.timekiller.model.entity.Letter
 
 @InjectViewState
 class MainPresenter : MvpPresenter<MainView>() {
 
-    fun testWord() {
-        val l1 = Letter('A', true)
-        val l2 = Letter('B', true)
-        val l3 = Letter('C', true)
-        val l4 = Letter('D', true)
+    fun wordLetterClicked(letter: Letter) {
+        log("letter ${letter.char.toString()} in word clicked")
+    }
 
-        viewState.setWord(listOf(l1, l2, l3, l4))
+    fun fieldLetterClicked(letter: Letter) {
+        log("letter ${letter.char.toString()} in field clicked")
+    }
+
+    fun testWord() {
+        showTestWord()
+        showTestField()
+    }
+
+
+
+
+    private fun showTestWord() {
+        val l1 = Letter('С', false)
+        val l2 = Letter('Л', false)
+        val l3 = Letter('О', false)
+        val l4 = Letter('В', false)
+        val l5 = Letter('О', false)
+
+        viewState.setWord(listOf(l1, l2, l3, l4, l5))
+    }
+
+    private fun showTestField() {
+        val l01 = Letter('С', false)
+        val l02 = Letter('Л', false)
+        val l03 = Letter('О', false)
+        val l04 = Letter('В', false)
+        val l05 = Letter('О', true)
+        val l06 = Letter('С', true)
+        val l07 = Letter('Л', false)
+        val l08 = Letter('О', true)
+        val l09 = Letter('В', false)
+        val l10 = Letter('О', true)
+
+        val l11 = Letter('С', true)
+        val l12 = Letter('Л', false)
+        val l13 = Letter('О', true)
+        val l14 = Letter('В', false)
+        val l15 = Letter('О', true)
+        val l16 = Letter('С', true)
+        val l17 = Letter('Л', false)
+        val l18 = Letter('О', true)
+        val l19 = Letter('В', true)
+        val l20 = Letter('О', false)
+
+        val l21 = Letter('С', true)
+        val l22 = Letter('Л', true)
+        val l23 = Letter('О', true)
+        val l24 = Letter('В', false)
+        val l25 = Letter('О', true)
+        val l26 = Letter('С', true)
+        val l27 = Letter('Л', false)
+        val l28 = Letter('О', true)
+        val l29 = Letter('В', true)
+        val l30 = Letter('О', true)
+
+        val l31 = Letter('С', true)
+        val l32 = Letter('Л', true)
+        val l33 = Letter('О', false)
+        val l34 = Letter('В', true)
+        val l35 = Letter('О', true)
+        val l36 = Letter('С', false)
+        val l37 = Letter('Л', false)
+        val l38 = Letter('О', false)
+        val l39 = Letter('В', true)
+        val l40 = Letter('О', true)
+
+        val l41 = Letter('С', true)
+        val l42 = Letter('Л', true)
+        val l43 = Letter('О', true)
+        val l44 = Letter('В', false)
+        val l45 = Letter('О', true)
+        val l46 = Letter('С', false)
+        val l47 = Letter('Л', false)
+        val l48 = Letter('О', true)
+        val l49 = Letter('В', false)
+        val l50 = Letter('О', true)
+
+        val l51 = Letter('С', false)
+        val l52 = Letter('Л', false)
+        val l53 = Letter('О', false)
+        val l54 = Letter('В', true)
+        val l55 = Letter('О', false)
+        val l56 = Letter('С', true)
+        val l57 = Letter('Л', false)
+        val l58 = Letter('О', true)
+        val l59 = Letter('В', false)
+        val l60 = Letter('О', false)
+
+        val l61 = Letter('С', false)
+        val l62 = Letter('Л', false)
+        val l63 = Letter('О', false)
+        val l64 = Letter('В', false)
+
+        viewState.setField(
+                arrayListOf(
+                        l01,l02,l03,l04,l05,l06,l07,l08,l09,l10,
+                        l11,l12,l13,l14,l05,l06,l07,l08,l09,l10,
+                        l01,l02,l03,l04,l05,l06,l07,l08,l09,l10,
+                        l01,l02,l03,l04,l05,l06,l07,l08,l09,l10,
+                        l01,l02,l03,l04,l05,l06,l07,l08,l09,l10,
+                        l01,l02,l03,l04,l05,l06,l07,l08,l09,l10,
+                        l01,l02,l03,l04
+                )
+        )
     }
 }
